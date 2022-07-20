8c4875d449979b1431792b983da34c55a3c0d4bd
nickderaj <nickderaj@gmail.com>
2022-07-20 17:24:39 +0800

    feat: created simple express server

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,23 @@
 'use strict';
 
-console.log('hello');
+import express, { Express, Request, Response } from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+const app: Express = express();
+dotenv.config();
+
+app.use(cors());
+app.use(express.json());
+
+const port = process.env.PORT || 3000;
+
+// Health Check
+app.get('/', (req: Request, res: Response) => {
+  res.status(200).json({
+    message: 'OK',
+    version: require('../package.json').version,
+  });
+});
+
+app.listen(port, () => console.log(`Running on port ${port}`));
